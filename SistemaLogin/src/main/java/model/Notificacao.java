7d7e2477eebdc0314be56d5c722b5a2d613fa591
Thobias Dantas <thobias.dantas@edu.ufes.br>
2025-12-06 00:21:10 -0300

    Feat(model): Criacao de notificacao

--- SistemaLogin/src/main/java/model/Notificacao.java
+++ SistemaLogin/src/main/java/model/Notificacao.java
@@ -4,10 +4,73 @@
  */
 package model;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author Ayler
  */
 public class Notificacao {
+    private int id;
+    private int idRemetente;
+    private int idDestinatario;
+    private String mensagem;
+    private LocalDateTime dataEnvio;
+    private boolean lida;
+
+    public Notificacao(int id, int idRemetente, int idDestinatario, String mensagem, LocalDateTime dataEnvio, boolean lida) {
+        this.id = id;
+        this.idRemetente = idRemetente;
+        this.idDestinatario = idDestinatario;
+        this.mensagem = mensagem;
+        this.dataEnvio = dataEnvio;
+        this.lida = lida;
+    }
+    
+    public Notificacao(){
+       
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getIdRemetente(){
+        return idRemetente;
+    }
+    public void setIdRemetente(int idRemetente){
+        this.idRemetente = idRemetente;
+    }
+    public int getIdDestinatario(){
+        return idDestinatario;
+    }
+    public void setIdDestinatario(int idDestinatario){
+        this.idDestinatario = idDestinatario;
+    }
+    public String getMensagem(){
+        return mensagem;
+    }
+    public void setMensagem(String mensagem){
+        this.mensagem = mensagem;
+    }
+    public LocalDateTime getDataEnvio(){
+        return dataEnvio;
+    }
+    public void setDataEnvio(LocalDateTime dataEnvio){
+        this.dataEnvio = dataEnvio;
+    }
+    public boolean isLida(){
+        return lida;
+    }
+    public void setLida(boolean lida ){
+        this.lida = lida;
+    }
+    @Override
+    public String toString(){
+        return mensagem;
+    }
+    
+    
     
 }
